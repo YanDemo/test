9402de20693e144ea29786c292ca976e64014f53
yanshang <yanshang@qq.com>
2018-10-17 22:10:03 +0800

    修改

--- idea-git-test/src/com/itheima/test.java
+++ idea-git-test/src/com/itheima/test.java
@@ -3,13 +3,12 @@ package com.itheima;
 public class test {
     public static void main(String[] args) {
         System.out.println("帅得掉渣!!!真丑");
-        System.out.println("帅得掉渣!!!真丑");
-        System.out.println("帅得掉渣!!!真丑");
 
     }
     public void test(){
         System.out.println("谁说的???");
-
+        System.out.println("谁说的???");
+        System.out.println("谁说的???");
 
     }
 }
